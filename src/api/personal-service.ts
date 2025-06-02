b30a3c2623defc78e6cdfda8df10bd999c348e5c
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-06-02 11:27:08 +0500

    api binding

--- src/api/personal-service.ts
+++ src/api/personal-service.ts
@@ -1,6 +1,5 @@
 import { PersonalServiceData, PersonalServiceResponse } from '@/types/personal-service/personal-service';
 import api from '../lib/api';
-import { ApplePayData, ApplePayResponse } from '@/types/apple-pay/apple-pay';
 
 export async function fetchPersonalService(): Promise<PersonalServiceData> {
     const { data } = await api.get<PersonalServiceResponse>('/personal-service');
